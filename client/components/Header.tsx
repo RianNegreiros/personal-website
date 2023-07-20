22972225fc3cadc1af2765187c0219bc7f3f2645
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-07-20 13:42:50 -0300

    refactor: change folder structure

---
+++ client/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header>
+    <a href="/" className="headerLinkLogo">My Blog</a>
+    <nav className="headerNav">
+      <a href="/login">Login</a>
+      <a href="/register">Register</a>
+    </nav>
+  </header>
+  )
+}
