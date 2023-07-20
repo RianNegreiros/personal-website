cef219a746a61cbe34edcc19c8368657a91d4963
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-07-20 12:44:38 -0300

    refactor: create post component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Post from './components/post'
 import styles from './page.module.css'
 
 export default function Home() {
@@ -10,19 +11,7 @@ export default function Home() {
           <a href="">Register</a>
         </nav>
       </header>
-      <div className={styles.divPost}>
-        <div>
-        <img src="https://picsum.photos/1920/1080" alt="hero" />
-        </div>
-        <div className={styles.divTexts}>
-          <h2>Lorem ipsum dolor sit.</h2>
-          <p className={styles.pInfo}>
-            <a className={styles.linkAuthor}>Rian Negreiros</a>
-            <time>2023-07-20 12:00</time>
-          </p>
-          <p className={styles.pSummary}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam numquam mollitia sed praesentium, consequuntur enim libero, eum iure quis officiis ducimus, inventore consequatur voluptatibus. Accusamus explicabo voluptas animi minus officia.</p>
-        </div>
-      </div>
+      <Post />
     </main>
   )
 }
