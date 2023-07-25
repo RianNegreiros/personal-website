c0b4a5ca51239934359ffa6e83ace590db9fea42
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-07-25 14:22:11 -0300

    fix: remove duplicated code

--- backend/API/Extensions/ApplicationServicesExtensions.cs
+++ backend/API/Extensions/ApplicationServicesExtensions.cs
@@ -1,7 +1,5 @@
 using backend.Core.Interfaces.Services;
-using Microsoft.EntityFrameworkCore;
 using backend.Infrastructure.Services;
-using backend.Infrastructure.Data;
 
 namespace backend.API.Extensions;
 
@@ -13,24 +11,6 @@ public static class ApplicationServicesExtensions
     services.AddScoped<IUserService, UserService>();
     services.AddScoped<IPostService, PostService>();
     services.AddScoped<CloudinaryService>();
-
-    services.AddDbContext<IdentityDbContext>(opt =>
-{
-  var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-  string connStr;
-
-  if (env == "Development")
-  {
-    connStr = config.GetConnectionString("DefaultConnection");
-  }
-  else
-  {
-    connStr = config.GetConnectionString("IdentityConnection");
-  }
-
-  opt.UseNpgsql(connStr);
-});
 
     return services;
   }
