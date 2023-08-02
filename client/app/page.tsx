46024aa776d9f04499d96d57f0deee1b7cb3cc4c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-02 15:52:22 -0300

    feat: add all posts index page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,52 @@
-export default function IndexPage() {
-  return <div>Index Page</div>
+import Link from "next/link";
+import { Post } from "./models";
+
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+}
+
+export default async function IndexPage() {
+  const data = (await getData()) as Post[];
+
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          All Posts
+        </h1>
+      </div>
+
+      <ul>
+        {data.map((post) => (
+          <li key={post.id} className="py-4">
+            <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+              <div>
+                <p className="text-base font-medium leading-6 text-teal-500">
+                  {new Date().toISOString().split("T")[0]}
+                </p>
+              </div>
+
+              <Link href={`/post/${post.id}`}
+                prefetch
+                className="space-y-3 xl:col-span-3"
+              >
+                <div>
+                  <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                    {post.title}
+                  </h3>
+                </div>
+
+
+                <p className="prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2">
+                  {post.body.split(" ").slice(0, 4).join(" ")}
+                </p>
+              </Link>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
