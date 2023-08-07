d4bf8bb6f9d227f22c5453b7c59708b5ea0262a2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-07 17:47:34 -0300

    feat: persist if user is logged in to local storage

--- client/app/contexts/AuthContext.tsx
+++ client/app/contexts/AuthContext.tsx
@@ -1,5 +1,4 @@
-// AuthContext.tsx
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useEffect, useState, ReactNode } from "react";
 
 type AuthContextType = {
   isLoggedIn: boolean;
@@ -9,7 +8,18 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Initialize the state from localStorage when running on the client-side
+    const storedState = localStorage.getItem("isLoggedIn");
+    setIsLoggedIn(storedState ? JSON.parse(storedState) : false);
+  }, []); // Empty dependency array to ensure this runs only once on mount, not during SSR
+
+  useEffect(() => {
+    // Save the state to localStorage whenever it changes
+    localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
