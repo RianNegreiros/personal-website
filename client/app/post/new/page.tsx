e0cc3a72b2bbb5ed24c445550fd224af0be8de66
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-08 08:34:14 -0300

    chore: remove quill package

--- client/app/post/new/page.tsx
+++ client/app/post/new/page.tsx
@@ -1,4 +1,25 @@
+"use client"
+
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+interface NewPostState {
+  editorContent: string;
+}
+
 export default function NewPostPage() {
+  const [state, setState] = useState<NewPostState>({
+    editorContent: '',
+  });
+
+  const handleEditorChange = (content: string) => {
+    setState({
+      ...state,
+      editorContent: content,
+    });
+  };
+
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
@@ -16,7 +37,12 @@ export default function NewPostPage() {
 
             <div className="sm:col-span-2">
               <label htmlFor="content" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
-              <textarea id="description" rows={16} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Place the markdown text here"></textarea>
+              <ReactQuill
+                value={state.editorContent}
+                onChange={handleEditorChange}
+                theme="snow"
+                placeholder="Place the markdown text here"
+              />
             </div>
           </div>
           <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 bg-teal-500 hover:bg-teal-600 focus:outline-none focus:bg-teal-600">
