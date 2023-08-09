b40ea882cf8ca23d39bb92fcc33623a5e9866dbf
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-09 19:28:09 -0300

    feat: create projects page

---
+++ client/app/projects/page.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image"
+
+interface Data {
+  id: string;
+  title: string;
+  overview: string;
+  url: string;
+  imageUrl: string;
+}
+
+async function getProjects() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/projects`);
+  const data = await response.json();
+
+  return data;
+}
+
+export default async function Projects() {
+  const data: Data[] = await getProjects();
+
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Todos projetos
+        </h1>
+      </div>
+
+      <div className="grid gap-y-8 sm:gap-6  sm:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-10 pt-8">
+        {data.map((project) => (
+          <article
+            key={project.id}
+            className="overflow-hidden dark:border-zinc-600 rounded-lg border border-gray-100 bg-white shadow-lg dark:bg-black dark:shadow-gray-700 shadow-teal-100"
+          >
+            <div className="h-56 w-full relative">
+              <Image
+                fill
+                src={project.imageUrl}
+                alt="Image of the project"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <div className="p-4 sm:p-6">
+              <a href={project.url} target="_blank">
+                <h3 className="text-lg font-medium text-gray-900 dark:text-white">
+                  {project.title}
+                </h3>
+              </a>
+
+              <p className=" line-clamp-3 mt-2 text-sm leading-relaxed text-gray-500 dark:text-gray-400">
+                {project.overview}
+              </p>
+
+              <a
+                href={project.url}
+                target="_blank"
+                className="cursor-pointer group mt-4 inline-flex items-center gap-1 text-sm font-medium text-teal-500"
+              >
+                Saiba mais
+                <span className="block transition-all group-hover:ms-0.5">
+                  &rarr;
+                </span>
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+}
