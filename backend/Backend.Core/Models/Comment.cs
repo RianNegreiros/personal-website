799798ce759b243b6ac60eed070b81f16fd7f561
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-11 09:09:34 -0300

    feat: create comments model

---
+++ backend/Backend.Core/Models/Comment.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Backend.Core.Models;
+
+public class Comment
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    [BsonElement("content")]
+    public string Content { get; set; }
+    [BsonElement("author")]
+    public string Author { get; set; }
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+}
--- backend/Backend.Core/Models/Post.cs
+++ backend/Backend.Core/Models/Post.cs
@@ -8,10 +8,13 @@ public class Post
   [BsonId]
   [BsonRepresentation(BsonType.ObjectId)]
   public string Id { get; set; }
-
+  [BsonElement("title")]
   public string Title { get; set; }
+  [BsonElement("summary")]
   public string Summary { get; set; }
+  [BsonElement("content")]
   public string Content { get; set; }
+  [BsonElement("author")]
   public User Author { get; set; }
 
   [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
@@ -19,4 +22,7 @@ public class Post
 
   [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
   public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+  [BsonElement("comments")]
+  public List<Comment> Comments { get; set; } = new List<Comment>();
 }
