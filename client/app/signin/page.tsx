fc845f2edc96b424219b3dacba151b2f5ccb0cd9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-12 14:42:16 -0300

    refactor: sign in page

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -5,6 +5,7 @@ import { SignInData } from "../models"
 import { signInUser } from "../utils/api";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../contexts/AuthContext";
+import Link from "next/link";
 
 export default function SignInPage() {
   const [formData, setFormData] = useState<SignInData>({
@@ -16,7 +17,7 @@ export default function SignInPage() {
   })
 
   const router = useRouter();
-  const { isAdmin, setIsAdmin } = useAuth();
+  const { setIsAdmin, setIsLogged } = useAuth();
 
   const handleInputChange = (event: FormEvent<HTMLInputElement>) => {
     const { name, value, type, checked } = event.currentTarget;
@@ -32,6 +33,7 @@ export default function SignInPage() {
       console.log("Sign in successful!");
 
       setIsAdmin(data.isAdmin)
+      setIsLogged(true);
 
       if (formData.rememberMe) {
         localStorage.setItem('token', data.token);
@@ -46,69 +48,84 @@ export default function SignInPage() {
   }
 
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 bg-white dark:bg-gray-50 rounded-md shadow-md lg:max-w-xl">
-        <h1 className="text-3xl font-bold text-center text-gray-900">Acesse sua conta</h1>
-        <form className="mt-6" onSubmit={handleSignIn}>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold text-gray-900"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@mail.com"
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-gray-900"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </div>
-          <div className="mt-2">
-            <div className="flex items-start mb-6">
-              <div className="flex items-center h-5">
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Acesse sua conta
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSignIn}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Seu email
+                </label>
                 <input
-                  id="remember"
-                  type="checkbox"
-                  name="rememberMe"
-                  checked={formData.rememberMe}
+                  type="email"
+                  name="email"
+                  value={formData.email}
                   onChange={handleInputChange}
-                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@mail.com"
                 />
               </div>
-              <label
-                htmlFor="remember"
-                className="ml-2 text-sm font-semibold text-gray-900"
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Sua senha
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      type="checkbox"
+                      name="rememberMe"
+                      checked={formData.rememberMe}
+                      onChange={handleInputChange}
+                      aria-describedby="remember"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="remember"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      Lembrar de mim
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-teal-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Remember me
-              </label>
-            </div>
-            <button
-              type="submit"
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-teal-500 rounded-md hover:bg-teal-600 focus:outline-none focus:bg-teal-600">
-              Login
-            </button>
+                Login
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Ainda não tem uma conta ? <Link href="/signup" className="font-medium text-teal-600 hover:underline dark:text-primary-500">Registre-se</Link>
+              </p>
+            </form>
           </div>
-        </form>
-
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
