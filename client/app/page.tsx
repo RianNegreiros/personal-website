b6fbd3bb4609a5e9063e67d79364669db124d106
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-14 15:38:11 -0300

    feat: sort posts

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,21 +1,59 @@
+"use client"
+
 import Link from "next/link";
 import { Post } from "./models";
 import { getPosts } from "./utils/api";
+import { useEffect, useState } from "react";
 
 async function getData() {
   return await getPosts();
 }
 
-export default async function BlogPage() {
-  const data = (await getData()) as Post[];
+export default function BlogPage() {
+  const [data, setData] = useState<Post[]>([]);
+  const [sortBy, setSortBy] = useState<"newest" | "oldest">("newest");
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedData = await getData();
+      setData(fetchedData);
+    }
+    fetchData();
+  }, []);
+
+  const handleSortChange = (value: "newest" | "oldest") => {
+    setSortBy(value);
+  };
+
+  const sortedData = data.slice().sort((a, b) => {
+    if (sortBy === "oldest") {
+      return a.createdAt.localeCompare(b.createdAt);
+    } else {
+      return b.createdAt.localeCompare(a.createdAt);
+    }
+  });
 
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <div>
+          <label htmlFor="sortSelect" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+          Ordenar por:
+          </label>
+          <select
+            id="sortSelect"
+            className="mt-1 block w-full p-2 border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            onChange={(e) => handleSortChange(e.target.value as "newest" | "oldest")}
+            value={sortBy}
+          >
+            <option value="newest">Mais recente</option>
+            <option value="oldest">Mais antigo</option>
+          </select>
+        </div>
       </div>
 
       <ul>
-        {data.map((post) => (
+        {sortedData.map((post) => (
           <li key={post.id} className="py-4">
             <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
               <div>
@@ -33,7 +71,6 @@ export default async function BlogPage() {
                     {post.title}
                   </h3>
                 </div>
-
 
                 <p className="prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2">
                   {post.summary}
