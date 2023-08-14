2368ff2d101eb674261dbc2f5caedb9602d71910
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-14 11:47:11 -0300

    feat: return slug

--- backend/Backend.Application/Models/PostViewModel.cs
+++ backend/Backend.Application/Models/PostViewModel.cs
@@ -6,5 +6,6 @@ public class PostViewModel
   public string Title { get; set; }
   public string Summary { get; set; }
   public string Content { get; set; }
+  public string Slug { get; set; }
   public DateTime CreatedAt { get; set; }
 }
