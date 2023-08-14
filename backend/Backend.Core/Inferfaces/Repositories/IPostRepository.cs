e9785eb929f41c2bb32820b830c65e5bc3484375
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-14 10:55:22 -0300

    feat: add getPostBySlug method to post repository

--- backend/Backend.Core/Inferfaces/Repositories/IPostRepository.cs
+++ backend/Backend.Core/Inferfaces/Repositories/IPostRepository.cs
@@ -7,5 +7,6 @@ public interface IPostRepository
     Task<Post> Create(Post post);
     Task<Post> Update(Post post);
     Task<Post> GetById(string id);
+    Task<Post> GetBySlug(string slug);
     Task<List<Post>> GetAll();
 }
