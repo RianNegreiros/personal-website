77a8d54a99c070bef890aed814831ea397058d78
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-09-28 17:51:30 -0300

    build: add sitemap and robots.txt on postbuild

---
+++ client/next-sitemap.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://riannegreiros.dev',
+  generateRobotsTxt: true,
+  exclude: ['/signin', '/signup'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/signin', '/signup'],
+      },
+    ],
+  },
+}
