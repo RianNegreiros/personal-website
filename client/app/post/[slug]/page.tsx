1213ac9618a306f1eaea2f487c13fc99db711f24
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-09-29 13:48:22 -0300

    fix: remove border
    remove gray border from comments form for a more clean style

--- client/app/post/[slug]/page.tsx
+++ client/app/post/[slug]/page.tsx
@@ -131,7 +131,7 @@ export default function PostPage({ params }: { params: { slug: string } }) {
       </ol>
 
       <form onSubmit={handleSubmit}>
-        <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <div className="w-full mb-4 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
           <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
             <label
               htmlFor="content"
