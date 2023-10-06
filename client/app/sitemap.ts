e38d7981d6b3d373bcc8802f34700e5dd2d304b3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-10-06 13:52:15 -0300

    fix: sitemap

--- client/app/sitemap.ts
+++ client/app/sitemap.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
 import siteMetadata from "./utils/siteMetaData";
+import { getFeed } from "./utils/api";
 
 export default async function sitemap() {
-  const response = await axios.get(process.env.NEXT_PUBLIC_API_URL + "/post");
-  const data = response.data.data;
+  const response = await getFeed();
+  const data = response.data;
 
-  const posts = data.items.map(({ slug, createdAt }: { slug: string, createdAt: string } ) => ({
+  const posts = data.map(({ slug, createdAt }: { slug: string, createdAt: string } ) => ({
     url: `${siteMetadata.siteUrl}/post/${slug}`,
     lastModified: createdAt,
   }));
 
-  const routes = ["", "/projects"].map((route) => ({
+  const routes = ["", "/projects", "/post"].map((route) => ({
     url: `${siteMetadata.siteUrl}${route}`,
     lastModified: new Date().toISOString(),
   }));
