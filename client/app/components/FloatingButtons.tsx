e27349563dcf8300ed069228e3124e566d579b2c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-11 07:24:16 -0300

    feat: add curriculum vitae link

--- client/app/components/FloatingButtons.tsx
+++ client/app/components/FloatingButtons.tsx
@@ -16,6 +16,15 @@ export default function FloatingButtons() {
     <div
       className={`fixed bottom-10 right-10 z-50 flex flex-col gap-2`}
     >
+      <Link href="https://cv.riannegreiros.dev" target="_blank"
+        className={`w-10 h-10 dark:bg-gray-800 rounded-full flex justify-center items-center`}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          className="w-8 h-8 text-teal-500 hover:text-teal-600" viewBox="0 0 1024 1024">
+          <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM380 696c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40zm0-144c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40zm0-144c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40zm304 272c0 4.4-3.6 8-8 8H492c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h184c4.4 0 8 3.6 8 8v48zm0-144c0 4.4-3.6 8-8 8H492c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h184c4.4 0 8 3.6 8 8v48zm0-144c0 4.4-3.6 8-8 8H492c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h184c4.4 0 8 3.6 8 8v48z" />
+        </svg>
+      </Link>
       <Link
         href="https://github.com/RianNegreiros" target="_blank"
         className={`w-10 h-10 dark:bg-gray-800 rounded-full flex justify-center items-center`}
