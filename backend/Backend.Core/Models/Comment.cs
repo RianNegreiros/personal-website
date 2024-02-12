6cc24b28b2cda5b422edc968667a8563d3e310cf
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-12 19:11:50 -0300

    refactor: use lowerCamelCase convention

--- backend/Backend.Core/Models/Comment.cs
+++ backend/Backend.Core/Models/Comment.cs
@@ -8,14 +8,19 @@ public class Comment
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
+
     [BsonElement("content")]
     public string Content { get; set; }
+
     [BsonElement("author")]
     public User Author { get; set; }
+
     [BsonElement("postId")]
     public string PostId { get; set; }
+
     [BsonElement("postSlug")]
     public string PostSlug { get; set; }
+
     [BsonElement("createdAt")]
     public DateTime CreatedAt { get; set; }
 }
--- backend/Backend.Core/Models/Project.cs
+++ backend/Backend.Core/Models/Project.cs
@@ -9,19 +9,27 @@ public class Project
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
 
+    [BsonElement("title")]
     public string Title { get; set; }
+
+    [BsonElement("url")]
 
     public string Url { get; set; }
 
+    [BsonElement("overview")]
     public string Overview { get; set; }
 
+    [BsonElement("imageUrl")]
     public string ImageUrl { get; set; }
 
+    [BsonElement("author")]
     public User Author { get; set; }
 
+    [BsonElement("createdAt")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
     public DateTime CreatedAt { get; set; }
 
+    [BsonElement("updatedAt")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
