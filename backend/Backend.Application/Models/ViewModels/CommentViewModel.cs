33c51ac1b6d1fb3594cad37c5e0cbed4ce3170b4
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-17 08:51:03 -0300

    refactor: return author id, username and email instead object
    avoid return sensitive data when returning the whole object

--- backend/Backend.Application/Models/ViewModels/CommentViewModel.cs
+++ backend/Backend.Application/Models/ViewModels/CommentViewModel.cs
@@ -1,4 +1,3 @@
-using Backend.Core.Models;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace Backend.Application.Models.ViewModels;
@@ -7,10 +6,28 @@ public class CommentViewModel : BaseCommentModel
 {
     [SwaggerSchema(Description = "The comment's id")]
     public string Id { get; set; }
-    
-    [SwaggerSchema(Description = "The comment's author")]
-    public User Author { get; set; }
+
+    [SwaggerSchema(Description = "The comment's post id")]
+    public string? PostId { get; set; }
+
+    [SwaggerSchema(Description = "The comment's post slug")]
+    public string? PostSlug { get; set; }
+
+    [SwaggerSchema(Description = "The comment's author id")]
+    public string AuthorId { get; set; }
+
+    [SwaggerSchema(Description = "The comment's author email")]
+    public string AuthorEmail { get; set; }
+
+    [SwaggerSchema(Description = "The comment's author username")]
+    public string AuthorUsername { get; set; }
+
+    [SwaggerSchema(Description = "The comment's replies")]
+    public List<CommentViewModel>? Replies { get; set; }
 
     [SwaggerSchema(Description = "The comment's creation date")]
     public DateTime CreatedAt { get; set; }
+
+    [SwaggerSchema(Description = "The comment's last change date")]
+    public DateTime UpdatedAt { get; set; }
 }
