957e3d12a0b2819a7718d25129855d1b697370d0
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-20 08:50:48 -0300

    fix: #219

--- client/app/projects/new/page.tsx
+++ client/app/projects/new/page.tsx
@@ -47,13 +47,13 @@ export default function NewProjectPage() {
         <form onSubmit={handleSubmit}>
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
             <div className="sm:col-span-2">
-              <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo</label>
+              <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Título</label>
               <input
                 type="text"
                 name="title"
                 id="title"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dracula-pink-600 focus:border-dracula-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dracula-pink dark:focus:border-dracula-pink"
-                placeholder="Type project title"
+                placeholder="Título do projeto"
                 required
                 onChange={handleChange}
               />
@@ -66,38 +66,46 @@ export default function NewProjectPage() {
                 name="overview"
                 id="overview"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dracula-pink focus:border-dracula-pink block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dracula-pink dark:focus:border-dracula-pink"
-                placeholder="Type project overview"
+                placeholder="Visão geral do projeto"
                 required
                 onChange={handleChange}
               />
             </div>
 
             <div className="sm:col-span-2">
-              <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Url</label>
+              <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">URL</label>
               <input
                 type="text"
                 name="url"
                 id="url"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dracula-pink-600 focus:border-dracula-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-400 dark:focus:ring-dracula-pink dark:focus:border-dracula-pink"
-                placeholder="Type project url"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dracula-pink-600 focus:border-dracula-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dracula-pink dark:focus:border-dracula-pink"
+                placeholder="URL para o projeto, GitHub ou deploy"
                 required
                 onChange={handleChange}
               />
             </div>
 
             <div className="sm:col-span-2">
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="image">Fazer Upload de arquivo</label>
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="image"
+              >
+                Arquivo
+              </label>
               <input
-                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                aria-describedby="project-image-description"
-                id="image"
                 type="file"
-                required
+                id="image"
                 name="image"
+                className="block w-full text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg py-2 px-3 
+            hover:shadow-md focus:ring-2 focus:ring-inset focus:ring-dracula-pink-500 focus:outline-none
+            dark:bg-gray-700 dark:border-gray-600 dark:text-gray-400 dark:hover:shadow-dark"
+                required
                 onChange={handleChange}
               />
+              <p className="mt-1 text-sm text-gray-300 dark:text-gray-300" id="file_input_help">
+                SVG, PNG or JPG.
+              </p>
             </div>
-
           </div>
           <button
             type="submit"
@@ -106,8 +114,8 @@ export default function NewProjectPage() {
           >
             {isPublishing ? 'Publicando...' : 'Publicar'}
           </button>
-        </form>
-      </div>
-    </section>
+        </form >
+      </div >
+    </section >
   )
 }
