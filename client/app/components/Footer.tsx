7be05c2e9c1ff34c2fd4017958a4a76cd766ec89
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-26 08:29:40 -0300

    feat: improve gray color contrast ratio on footer

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
       <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
         <hr className='my-6 border-dracula-aro-200 dark:border-dracula-aro-700 sm:mx-auto lg:my-8' />
         <div className='sm:flex sm:items-center sm:justify-between'>
-          <span className='text-sm text-dracula-aro-500 dark:text-dracula-aro-400 sm:text-center'>
+          <span className='text-sm text-dracula-aro-700 dark:text-dracula-aro-400 sm:text-center'>
             © {new Date().getFullYear()}{' '}
             <Link
               href='https://www.linkedin.com/in/riannegreiros'
@@ -29,7 +29,7 @@ export default function Footer() {
             . Todos os direitos reservados.
           </span>
 
-          <ul className='mb-6 flex flex-wrap items-center text-sm font-medium text-dracula-aro-500 dark:text-dracula-aro-400 sm:mb-0'>
+          <ul className='mb-6 flex flex-wrap items-center text-sm font-medium text-dracula-aro-700 dark:text-dracula-aro-400 sm:mb-0'>
             <li>
               <a
                 href='/terms/privacy'
