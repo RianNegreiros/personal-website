c2a1cbd3433cd50857cdd36149d75a72e853bd6d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-26 08:27:09 -0300

    feat: improve contrast ratio on footer

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -13,15 +13,15 @@ export default function Footer() {
   }
 
   return (
-    <footer className='mt-auto bg-white dark:bg-gray-900'>
+    <footer className='mt-auto bg-white dark:bg-dracula-aro-900'>
       <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
-        <hr className='my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8' />
+        <hr className='my-6 border-dracula-aro-200 dark:border-dracula-aro-700 sm:mx-auto lg:my-8' />
         <div className='sm:flex sm:items-center sm:justify-between'>
-          <span className='text-sm text-gray-500 dark:text-gray-400 sm:text-center'>
+          <span className='text-sm text-dracula-aro-500 dark:text-dracula-aro-400 sm:text-center'>
             © {new Date().getFullYear()}{' '}
             <Link
               href='https://www.linkedin.com/in/riannegreiros'
-              className='text-dracula-purple hover:underline'
+              className='text-dracula-purple-500 hover:underline'
               aria-label='Rodapé da página Abrir LinkedIn'
             >
               Rian Negreiros™
@@ -29,11 +29,11 @@ export default function Footer() {
             . Todos os direitos reservados.
           </span>
 
-          <ul className='mb-6 flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mb-0'>
+          <ul className='mb-6 flex flex-wrap items-center text-sm font-medium text-dracula-aro-500 dark:text-dracula-aro-400 sm:mb-0'>
             <li>
               <a
                 href='/terms/privacy'
-                className='mr-4 text-dracula-purple hover:underline md:mr-6'
+                className='mr-4 text-dracula-purple-500 hover:underline md:mr-6'
               >
                 Política Privacidade
               </a>
@@ -41,7 +41,7 @@ export default function Footer() {
             <li>
               <a
                 href='/terms/service'
-                className='mr-4 text-dracula-purple hover:underline md:mr-6'
+                className='mr-4 text-dracula-purple-500 hover:underline md:mr-6'
               >
                 Termos de Serviço
               </a>
