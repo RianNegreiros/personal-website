1293b0ba09070eee260083070e6f906bf24ee691
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-26 20:18:04 -0300

    refactor: rename from article to post

--- client/app/sitemap.ts
+++ client/app/sitemap.ts
@@ -11,9 +11,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const response = await getPosts(0, 0)
   const data: PostData[] = await response.data.items
 
-  return data.map((article) => ({
-    url: `${siteMetadata.siteUrl}/posts/${article.slug}`,
-    lastModified: article.updatedAt,
+  return data.map((post) => ({
+    url: `${siteMetadata.siteUrl}/posts/${post.slug}`,
+    lastModified: post.updatedAt,
     changeFrequency: 'weekly',
     priority: 0.5,
   }))
